e2516b160f1ac42dbbb5702dd5f0706a05e3ade6
serhiichaiuk <chayck7@gmail.com>
2022-05-22 22:28:19 +0300

    added a routes for the cars page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "@/views/HomeView";
+import CarsView from "@/views/CarsView";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    redirect: "/cars",
+    component: CarsView,
+  },
+  {
+    path: "/cars",
+    name: "cars",
+    component: CarsView,
   },
 ];
 
