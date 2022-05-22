efa30ef552fe19c722033a56d062e212b7b88e38
serhiichaiuk <chayck7@gmail.com>
2022-05-23 00:32:44 +0300

    added ReservationView file

---
+++ src/views/ReservationsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="pt-6">
+    <v-row>
+      <v-col cols="4"></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "ReservationsView",
+};
+</script>
+
+<style scoped></style>
