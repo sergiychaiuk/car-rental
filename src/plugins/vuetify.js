671a8ed2ed255d89269a60b7fddede0bff0059a3
serhiichaiuk <chayck7@gmail.com>
2022-05-22 22:17:21 +0300

    installed of ukr localization in vuetify components

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,7 +3,13 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
+import uk from "vuetify/lib/locale/uk";
+
 export default new Vuetify({
+  lang: {
+    locales: { uk },
+    current: "uk",
+  },
   icons: {
     iconfont: "mdi",
   },
